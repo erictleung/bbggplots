d444a0a22ca0f04dd37d31197023bd386e85b17a
Eric Leung <2754821+erictleung@users.noreply.github.com>
2025-05-10 11:13:22 -0400

    Add draft scraping scripts and data

---
+++ data-raw/pull_archived_data.R
@@ -0,0 +1,80 @@
+# Code to pull archival website snapshots from The Internet Archive
+# https://archive.org
+
+# Setup ----
+
+# Web
+library(rvest) # Easily Harvest (Scrape) Web Pages CRAN v1.0.4
+library(chromote) # Headless Chrome Web Browser Interface CRAN v0.5.0
+
+# Data wrangling
+library(dplyr) # A Grammar of Data Manipulation CRAN v1.1.4
+library(tidyr) # Tidy Messy Data CRAN v1.3.1
+library(stringr) # Simple, Consistent Wrappers for Common String Operations CRAN v1.5.1
+
+# Utils
+library(purrr) # Functional Programming Tools CRAN v1.0.2
+library(readr) # Read Rectangular Text Data CRAN v2.1.5
+library(forcats) # Tools for Working with Categorical Variables (Factors) CRAN v1.0.0
+library(glue) # Interpreted String Literals CRAN v1.7.0
+library(here)
+
+
+# Read current records -----
+# Read in records so far
+message("Reading in records so far.")
+f <- here("data-raw", "archived.csv")
+if (file.exists(f)) {
+  records <- read_csv(f)
+}
+
+print(date <- Sys.Date())
+
+
+# Webpages ----
+# https://web.archive.org/web/20240513105238/https://www.bbg.org/collections/cherries
+
+
+# Scrape website ----
+# Get archived webpages and let JavaScript code load elements
+flowers <- read_html_live("https://web.archive.org/web/20240513105238/https://www.bbg.org/collections/cherries")
+
+message(glue("Current date: {date}"))
+message(glue("{html_text(html_elements(flowers, 'figcaption'))} <- Should be same"))
+
+# Parse results ----
+message("Get all annotated trees and form them into a nice data frame")
+# Inspiration: https://stackoverflow.com/a/34513555/2468369
+df_flowers <-
+  flowers %>%
+  html_nodes("span.location") %>%
+  map(html_attrs) %>%
+  map_df(~ as.list(.))
+
+# Pre-process data
+bloom_lvls <- c("Prebloom", "First Bloom", "Peak Bloom", "Post-Peak Bloom")
+new_records <-
+  df_flowers %>%
+  separate_wider_delim(
+    cols = class,
+    delim = " ",
+    names = c("tree", "location", "bloom", "tooltip")
+  ) %>%
+  mutate(bloom = case_when(
+    bloom == "bloom0" ~ "Prebloom",
+    bloom == "bloom1" ~ "First Bloom",
+    bloom == "bloom2" ~ "Peak Bloom",
+    bloom == "bloom3" ~ "Post-Peak Bloom",
+    TRUE ~ "N/A"
+  )) %>%
+  separate_wider_regex(
+    cols = tree,
+    patterns = c(tree = "^[a-z_]*_", id = "[0-9]*$")
+  ) %>%
+  mutate(
+    tree = str_remove(tree, pattern = regex("_$")),
+    date = date,
+    id = as.integer(id),
+    bloom = bloom %>% fct_expand(bloom_lvls) %>% fct_relevel(bloom_lvls),
+  ) %>%
+  select(tree, id, bloom, date)
