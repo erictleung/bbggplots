d85faf4871998bfd41069ea1c031a1c6dbe451d6
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-04-02 00:31:29 -0400

    Simplify and update combine and save script
    Future proof this script to scale to any number of years when saving out
    data.

--- data-raw/combine_and_save.R
+++ data-raw/combine_and_save.R
@@ -1,6 +1,6 @@
 # Pull live data from the Brooklyn Botanical Garden
+# Rscript combined_and_save.R
 # https://www.bbg.org/collections/cherries
-
 
 # Setup ----
 
@@ -10,15 +10,21 @@ library(dplyr) # A Grammar of Data Manipulation CRAN v1.1.4
 # Utils
 library(readr) # Read Rectangular Text Data CRAN v2.1.5
 library(here) # A Simpler Way to Find Your Files CRAN v1.0.1
+library(stringr)
+library(purrr)
 
 
 # Read data ----
 
-records <- read_csv(here("data-raw/bbg_tree_bloom_2025.csv"))
+# Read all data all at once
+bbgdata <- here("data-raw") |>
+  list.files(pattern = "bbg_tree_bloom") |>
+  map_chr(\(x) str_c("data-raw/", x)) |>
+  map_dfr(read_csv)
 
+# Checks
+unique(bbgdata$date)
 
 # Save out data ----
-
-bbgdata <- records
 
 usethis::use_data(bbgdata, overwrite = TRUE)
