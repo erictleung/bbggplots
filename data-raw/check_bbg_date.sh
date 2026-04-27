288af91e90fbb28a7463aa2c165a06d4d16253e1
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-04-26 21:23:17 -0400

    Update check BBG site script with decision

--- data-raw/check_bbg_date.sh
+++ data-raw/check_bbg_date.sh
@@ -3,9 +3,18 @@
 curl https://www.bbg.org/collections/cherries >> bbg.html
 
 echo "Today's date: "
-date '+%B %-e, %Y'
+TODAY=`date '+%B %-e, %Y'`
+TODAY+="."  # Date for BBG has a period at the end
+echo $TODAY
 
 echo "BBG's date: "
-cat bbg.html | grep -oP '(?<=Last update: ).*?(?=<)'
+BBG=`cat bbg.html | grep -oP '(?<=Last update: ).*?(?=<)'`
+echo $BBG
+
+if [ "$TODAY" = "$BBG" ]; then
+    echo "Pull data!"
+else
+    echo "Wait to pull data!"
+fi
 
 rm bbg.html
